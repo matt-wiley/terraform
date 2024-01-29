8375306c53bced809b986289c5a71fb5967691bf
Matt Wiley <matt.wiley@8451.com>
2024-01-29 11:53:50 -0500

    Simplify workspace env config

--- env.sh
+++ env.sh
@@ -1,27 +1,2 @@
-
-ROOT_DIR="$(pwd)"
-
-# Source ARM environment variables
-source .local/arm_env.sh
-
-function drh {
-    if [[ -z "${@}" ]]; then
-        echo "No image name provided. Please provide an image name and, optionally, commands."
-    else
-        docker run -it --rm -v "$(pwd):$(pwd)" -w "$(pwd)" "${@}"
-    fi
-}
-
-function tooling_image {
-    local command_str="${@}"
-    drh \
-      -v "${ROOT_DIR}/.config/terraform:/root/.terraform.d" \
-      -v "${ROOT_DIR}/.config/azure:/root/.azure" \
-      mattwiley/tf:az-01 ${command_str}
-}
-
-
-alias terraform="tooling_image terraform"
+export AZURE_CONFIG_DIR="${ROOT_DIR}/.config/azure"
 alias tf="terraform"
-
-alias az="tooling_image az"
